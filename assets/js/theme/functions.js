12452261b87ba66c8711c5964ebdb83a64063f9e
Pedro Daniel de Oliveira Hoeller <pedro@arealocal.com.br>
2024-07-16 14:21:17 -0300

    Troca de tema e mobile

--- assets/js/theme/functions.js
+++ assets/js/theme/functions.js
@@ -1,6 +1,6 @@
 jQuery("document").ready(function(){
   AOS.init();
-  const sections = document.querySelectorAll('h2, section');
+  const sections = document.querySelectorAll('section');
   const navLinks = document.querySelectorAll('.nav-link');
   window.addEventListener('scroll', () => {
     let current = '';
@@ -20,8 +20,23 @@ jQuery("document").ready(function(){
   const swiperTec = new Swiper('.swiper-tecnologias', {
     slidesPerView: 12,
     spaceBetween: 12,
-    loop: true,
     breakpoints: {
+      320: {
+        slidesPerView: 4,
+        spaceBetween: 12
+      },
+      480: {
+        slidesPerView: 5,
+        spaceBetween: 12
+      },
+      768: {
+        slidesPerView: 8,
+        spaceBetween: 12
+      },
+      1000: {
+        slidesPerView: 10,
+        spaceBetween: 12
+      },
       1200: {
         slidesPerView: 12,
         spaceBetween: 12
@@ -58,7 +73,11 @@ jQuery("document").ready(function(){
   document.addEventListener('mousemove', function(){
     var x = event.clientX;
     var y = event.clientY;
-    jQuery(".pointer-background").css("background", "radial-gradient(600px at " + x +"px "+ y +"px, rgba(255, 255, 255, 0.05), transparent 80%)");
+    const rootStyles = getComputedStyle(document.body);
+    const whiteColor = rootStyles.getPropertyValue('--white').trim();
+    var op = whiteColor=="#FFF"?0.05:0.2;
+    const whiteWithOpacity = hexToRgba(whiteColor, op);
+    jQuery(".pointer-background").css("background", "radial-gradient(600px at " + x +"px "+ y +"px, "+ whiteWithOpacity +", transparent 80%)");
   });
   jQuery(".menu-icon").on("click", function(){
     jQuery(".lateral").addClass("active");
@@ -70,4 +89,38 @@ jQuery("document").ready(function(){
       jQuery(".initial-page").removeClass("recuo");
     }
   });
+  jQuery(".change-color-item").on("click", function(){
+    if(jQuery(this).attr("color")=="black"){
+      jQuery(this).attr("color", "white");
+      jQuery("body").css({
+        "--p-color": "#005543",
+        "--s-color": "#c3c3c3",
+        "--black": "#fff",
+        "--grey": "#000000",
+        "--white": "#141414"
+      });
+    }else{
+      jQuery(this).attr("color", "black");
+      jQuery("body").css({
+        "--p-color": "orangered",
+        "--s-color": "#333",
+        "--black": "#141414",
+        "--grey": "#e0e0e0",
+        "--white": "white"
+      });
+    }
+  });
 });
+function hexToRgba(hex, alpha) {
+  hex = hex.replace(/^#/, '');
+  if (hex.length === 3) {
+    hex = hex.split('').map(x => x + x).join('');
+  } else if (hex.length !== 6) {
+    throw new Error('O valor hexadecimal deve ter 3 ou 6 caracteres');
+  }
+  const bigint = parseInt(hex, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@
 
 jQuery("document").ready(function () {
   AOS.init();
-  var sections = document.querySelectorAll('h2, section');
+  var sections = document.querySelectorAll('section');
   var navLinks = document.querySelectorAll('.nav-link');
   window.addEventListener('scroll', function () {
     var current = '';
@@ -39,8 +39,23 @@ jQuery("document").ready(function () {
   var swiperTec = new Swiper('.swiper-tecnologias', {
     slidesPerView: 12,
     spaceBetween: 12,
-    loop: true,
     breakpoints: {
+      320: {
+        slidesPerView: 4,
+        spaceBetween: 12
+      },
+      480: {
+        slidesPerView: 5,
+        spaceBetween: 12
+      },
+      768: {
+        slidesPerView: 8,
+        spaceBetween: 12
+      },
+      1000: {
+        slidesPerView: 10,
+        spaceBetween: 12
+      },
       1200: {
         slidesPerView: 12,
         spaceBetween: 12
@@ -77,7 +92,11 @@ jQuery("document").ready(function () {
   document.addEventListener('mousemove', function () {
     var x = event.clientX;
     var y = event.clientY;
-    jQuery(".pointer-background").css("background", "radial-gradient(600px at " + x + "px " + y + "px, rgba(255, 255, 255, 0.05), transparent 80%)");
+    var rootStyles = getComputedStyle(document.body);
+    var whiteColor = rootStyles.getPropertyValue('--white').trim();
+    var op = whiteColor == "#FFF" ? 0.05 : 0.2;
+    var whiteWithOpacity = hexToRgba(whiteColor, op);
+    jQuery(".pointer-background").css("background", "radial-gradient(600px at " + x + "px " + y + "px, " + whiteWithOpacity + ", transparent 80%)");
   });
   jQuery(".menu-icon").on("click", function () {
     jQuery(".lateral").addClass("active");
@@ -89,7 +108,43 @@ jQuery("document").ready(function () {
       jQuery(".initial-page").removeClass("recuo");
     }
   });
+  jQuery(".change-color-item").on("click", function () {
+    if (jQuery(this).attr("color") == "black") {
+      jQuery(this).attr("color", "white");
+      jQuery("body").css({
+        "--p-color": "#005543",
+        "--s-color": "#c3c3c3",
+        "--black": "#fff",
+        "--grey": "#000000",
+        "--white": "#141414"
+      });
+    } else {
+      jQuery(this).attr("color", "black");
+      jQuery("body").css({
+        "--p-color": "orangered",
+        "--s-color": "#333",
+        "--black": "#141414",
+        "--grey": "#e0e0e0",
+        "--white": "white"
+      });
+    }
+  });
 });
+function hexToRgba(hex, alpha) {
+  hex = hex.replace(/^#/, '');
+  if (hex.length === 3) {
+    hex = hex.split('').map(function (x) {
+      return x + x;
+    }).join('');
+  } else if (hex.length !== 6) {
+    throw new Error('O valor hexadecimal deve ter 3 ou 6 caracteres');
+  }
+  var bigint = parseInt(hex, 16);
+  var r = bigint >> 16 & 255;
+  var g = bigint >> 8 & 255;
+  var b = bigint & 255;
+  return "rgba(".concat(r, ", ").concat(g, ", ").concat(b, ", ").concat(alpha, ")");
+}
 
 /***/ }),
 
